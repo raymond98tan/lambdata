dddbc5c9ce123028b297fc0ca70f1d04c13d4891
raymond98tan <raymond98tan@gmail.com>
2020-09-28 22:09:09 -0400

    asfsg

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setuptools.setup(
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
-    "Operating System :: Windows 10"
+    "Operating System :: OS Independent"
   ]
 )
